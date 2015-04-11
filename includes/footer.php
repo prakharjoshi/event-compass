100ce46ab2677bee7462b42a475801ef858157d3
Miten Shah <ninjamiten@gmail.com>
2015-04-11 18:15:15 +0530

    Indented footer

--- includes/footer.php
+++ includes/footer.php
@@ -8,54 +8,52 @@
 <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet">
 
 <footer>
- <div class="container text-center">
-    <hr />
-  <div class="row">
-    <div class="col-lg-12">
-      <div class="col-md-3">
-        <ul class="nav nav-pills nav-stacked">
-          <li><strong><span class="fa fa-users"></span> Team</strong></li>
-          <li><a href="#">About Us</a></li>
-          <li><a href="http://technologyforfirsttime.blogspot.in">Blog</a></li>
-        </ul>
-      </div>
-      <div class="col-md-3">
-        <ul class="nav nav-pills nav-stacked">
-          <li><strong><span class="fa fa-phone"></span> Contact Us</strong></li>
-          <li><a href="mailto:eventcompass22@gmail.com">E-mail</a></li>
-          <li><a href="https://twitter.com/eventcompass22">Twitter</a></li>
-          <li><a href="https://github.com/prakharjoshi/event-compass">Github</a></li>
-          <li><a href="https://plus.google.com/u/0/107984560752107119293/posts">Google+</a></li>
-        </ul>
-      </div>
-      <div class="col-md-3">
-        <ul class="nav nav-pills nav-stacked">
-          <li><strong><span class="fa fa-sitemap"></span> Navigate</strong></li>
-          <li><a href="login.php">Sign In</a></li>
-          <li><a href="register.php">Sign Up</a></li>
-          
-
-        </ul>
-      </div>
-      <div class="col-md-3">
-        <ul class="nav nav-pills nav-stacked">
-          <li><strong><span class="fa fa-support"></span> Support</strong></li>
-          <li><a href="#">Product Help</a></li>
-        </ul>
-      </div>  
-    </div>
-  </div>
-  <hr>
-    <div class="row">
-        <div class="col-lg-12">
-            <ul class="nav nav-pills nav-justified">
-                <li><a href="/"><span class="fa fa-copyright"></span> 2015 Event Compass</a></li>
-                <li><a href="#"><span class="fa fa-legal"></span> Terms of Service</a></li>
-                <li><a href="#"><span class="fa fa-lock"></span> Privacy</a></li>
-            </ul>
+    <div class="container text-center">
+        <hr>
+        <div class="row">
+            <div class="col-lg-12">
+            <div class="col-md-3">
+                <ul class="nav nav-pills nav-stacked">
+                    <li><strong><span class="fa fa-users"></span> Team</strong></li>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="http://technologyforfirsttime.blogspot.in">Blog</a></li>
+                </ul>
+            </div>
+            <div class="col-md-3">
+                <ul class="nav nav-pills nav-stacked">
+                    <li><strong><span class="fa fa-phone"></span> Contact Us</strong></li>
+                    <li><a href="mailto:eventcompass22@gmail.com">E-mail</a></li>
+                    <li><a href="https://twitter.com/eventcompass22">Twitter</a></li>
+                    <li><a href="https://github.com/prakharjoshi/event-compass">Github</a></li>
+                    <li><a href="https://plus.google.com/u/0/107984560752107119293/posts">Google+</a></li>
+                </ul>
+            </div>
+            <div class="col-md-3">
+                <ul class="nav nav-pills nav-stacked">
+                    <li><strong><span class="fa fa-sitemap"></span> Navigate</strong></li>
+                    <li><a href="login.php">Sign In</a></li>
+                    <li><a href="register.php">Sign Up</a></li>
+                </ul>
+            </div>
+            <div class="col-md-3">
+                <ul class="nav nav-pills nav-stacked">
+                    <li><strong><span class="fa fa-support"></span> Support</strong></li>
+                    <li><a href="#">Product Help</a></li>
+                </ul>
+            </div>  
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-lg-12">
+                <ul class="nav nav-pills nav-justified">
+                    <li><a href="/"><span class="fa fa-copyright"></span> 2015 Event Compass</a></li>
+                    <li><a href="#"><span class="fa fa-legal"></span> Terms of Service</a></li>
+                    <li><a href="#"><span class="fa fa-lock"></span> Privacy</a></li>
+                </ul>
+            </div>
         </div>
     </div>
-</div>
 </footer>
 
 
